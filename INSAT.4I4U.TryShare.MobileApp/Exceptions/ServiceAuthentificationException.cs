e98c9fde2f707bf3db9910ff28a9e3e2b5793353
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-12-01 09:20:40 +0100

    Add custom exceptions and Request provider interface and implementation

---
+++ INSAT.4I4U.TryShare.MobileApp/Exceptions/ServiceAuthentificationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSAT._4I4U.TryShare.MobileApp.Exceptions
+{
+    public class ServiceAuthentificationException : Exception
+    {
+        public string Content { get; }
+
+        public ServiceAuthentificationException() : base()
+        {
+        }
+
+        public ServiceAuthentificationException(string content) : base(content)
+        {   
+            Content = content;
+        }
+    }
+}
---
+++ INSAT.4I4U.TryShare.MobileApp/GlobalSettings.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSAT._4I4U.TryShare.MobileApp
+{
+    public class GlobalSettings
+    {
+        public const string AzureTag = "Azure";
+        public const string MockTag = "Mock";
+#if DEBUG
+        public const string DefaultEndpoint = "http://localhost:5000";
+#else
+        public const string DefaultEndpoint = "http://localhost:5000";
+#endif
+
+        private string _baseIdentityEndpoint;
+        private string _baseGatewayShoppingEndpoint;
+        private string _baseGatewayMarketingEndpoint;
+
+        public GlobalSettings()
+        {
+            AuthToken = "INSERT AUTHENTICATION TOKEN";
+
+            BaseIdentityEndpoint = DefaultEndpoint;
+            BaseGatewayShoppingEndpoint = DefaultEndpoint;
+        }
+
+        public static GlobalSettings Instance { get; } = new GlobalSettings();
+
+        public string BaseIdentityEndpoint
+        {
+            get => _baseIdentityEndpoint;
+            set
+            {
+                _baseIdentityEndpoint = value;
+                UpdateEndpoint(_baseIdentityEndpoint);
+            }
+        }
+
+        public string BaseGatewayShoppingEndpoint
+        {
+            get => _baseGatewayShoppingEndpoint;
+            set
+            {
+                _baseGatewayShoppingEndpoint = value;
+                UpdateGatewayShoppingEndpoint(_baseGatewayShoppingEndpoint);
+            }
+        }
+
+        public string ClientId { get; } = "xamarin";
+
+        public string ClientSecret { get; } = "secret";
+
+        public string AuthToken { get; set; }
+
+        public string RegisterWebsite { get; set; }
+
+        public string AuthorizeEndpoint { get; set; }
+
+        public string UserInfoEndpoint { get; set; }
+
+        public string TokenEndpoint { get; set; }
+
+        public string LogoutEndpoint { get; set; }
+
+        public string Callback { get; set; }
+
+        public string LogoutCallback { get; set; }
+
+        public string GatewayShoppingEndpoint { get; set; }
+
+        public string GatewayMarketingEndpoint { get; set; }
+
+        private void UpdateEndpoint(string endpoint)
+        {
+            RegisterWebsite = $"{endpoint}/Account/Register";
+            LogoutCallback = $"{endpoint}/Account/Redirecting";
+
+            var connectBaseEndpoint = $"{endpoint}/connect";
+            AuthorizeEndpoint = $"{connectBaseEndpoint}/authorize";
+            UserInfoEndpoint = $"{connectBaseEndpoint}/userinfo";
+            TokenEndpoint = $"{connectBaseEndpoint}/token";
+            LogoutEndpoint = $"{connectBaseEndpoint}/endsession";
+
+            var baseUri = GlobalSettings.ExtractBaseUri(endpoint);
+            Callback = $"{baseUri}/xamarincallback";
+        }
+
+        private void UpdateGatewayShoppingEndpoint(string endpoint)
+        {
+            GatewayShoppingEndpoint = endpoint;
+        }
+
+        private void UpdateGatewayMarketingEndpoint(string endpoint)
+        {
+            GatewayMarketingEndpoint = endpoint;
+        }
+
+        private static string ExtractBaseUri(string endpoint)
+        {
+            try
+            {
+                var uri = new Uri(endpoint);
+                var baseUri = uri.GetLeftPart(UriPartial.Authority);
+
+                return baseUri;
+            }
+            catch (Exception ex)
+            {
+                _ = ex;
+                return DefaultEndpoint;
+            }
+        }
+    }
+}
