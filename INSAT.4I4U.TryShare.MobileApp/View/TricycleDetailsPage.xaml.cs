4a751bfc2419204154cad475f93352f8a492a40b
Nicolas Perruisset <perruisset@insa-toulouse.fr>
2022-12-14 19:28:18 +0100

    create structure of detail page, try to add the address of the bike

--- INSAT.4I4U.TryShare.MobileApp/View/TricycleDetailsPage.xaml.cs
+++ INSAT.4I4U.TryShare.MobileApp/View/TricycleDetailsPage.xaml.cs
@@ -1,13 +1,25 @@
 using INSAT._4I4U.TryShare.MobileApp.Model;
+using Xamarin.Google.Crypto.Tink.Mac;
 
 namespace INSAT._4I4U.TryShare.MobileApp.View;
 
 [QueryProperty(nameof(Tricycle),"Tricycle")]
 public partial class TricycleDetailsPage: ContentPage
 {
+
+    TricycleDetailsViewModel _viewModel = new();
+
     public TricycleDetailsPage(TricycleDetailsViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
     }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await _viewModel.GetTricycleAddress();
+
+    }
+
 }
--- INSAT.4I4U.TryShare.MobileApp/ViewModel/TricycleDetailsViewModel.cs
+++ INSAT.4I4U.TryShare.MobileApp/ViewModel/TricycleDetailsViewModel.cs
@@ -6,12 +6,53 @@ using INSAT._4I4U.TryShare.MobileApp.ViewModel.Base;
 namespace INSAT._4I4U.TryShare.MobileApp.ViewModel
 {
     [QueryProperty(nameof(Tricycle), "Tricycle")]
-    public partial class TricycleDetailsViewModel : BaseViewModel
+    public partial class TricycleDetailsViewModel : BaseViewModel, IQueryAttributable
     {
-        public TricycleDetailsViewModel()
-        {
-        }
+
         [ObservableProperty]
         Tricycle tricycle;
+
+        public TricycleDetailsViewModel()
+        {
+            Console.WriteLine();
+            ApplyQueryAttributes(new Dictionary<string, object>
+                { {"Tricycle", tricycle } });
+        }
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            Tricycle = query["Tricycle"] as Tricycle;
+            OnPropertyChanged("Tricycle");
+        }
+
+        public async Task<string> GetTricycleAddress()
+        {
+            double latitude = Tricycle.Location.Latitude;
+            double longitude = Tricycle.Location.Longitude;
+
+            IEnumerable<Placemark> placemarks = await Geocoding.Default.GetPlacemarksAsync(latitude, longitude);
+
+            Placemark placemark = placemarks?.FirstOrDefault();
+
+            if (placemark is not null)
+            {
+                return
+                    $"AdminArea:       {placemark.AdminArea}\n" +
+                    $"CountryCode:     {placemark.CountryCode}\n" +
+                    $"CountryName:     {placemark.CountryName}\n" +
+                    $"FeatureName:     {placemark.FeatureName}\n" +
+                    $"Locality:        {placemark.Locality}\n" +
+                    $"PostalCode:      {placemark.PostalCode}\n" +
+                    $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                    $"SubLocality:     {placemark.SubLocality}\n" +
+                    $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                    $"Thoroughfare:    {placemark.Thoroughfare}\n";
+
+            }
+
+            return "";
+        }
+
+
     }
 }
