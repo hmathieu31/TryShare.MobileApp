f54039444ad743724f0342aeda921c18609857a6
Nicolas Perruisset <perruisset@insa-toulouse.fr>
2023-01-14 15:30:53 +0100

    create userservice

---
+++ INSAT.4I4U.TryShare.MobileApp/Services/User/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSAT._4I4U.TryShare.MobileApp.Services.User
+{
+    internal class UserService
+    {
+    }
+}
