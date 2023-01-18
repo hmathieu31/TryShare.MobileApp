27445939a65ebd8a8fde3ccfe1a4d98bfb486945
Hugo MATHIEU <hmathieu@etud.insa-toulouse.fr>
2023-01-18 10:15:31 +0000

    Apply suggestions from code review =>
    fix `NullReferenceException`

--- INSAT.4I4U.TryShare.MobileApp/Infrastructure/Mappers/UserIdentityMapper.cs
+++ INSAT.4I4U.TryShare.MobileApp/Infrastructure/Mappers/UserIdentityMapper.cs
@@ -21,7 +21,7 @@ namespace INSAT._4I4U.TryShare.MobileApp.Infrastructure.Mappers
         private static string? ReturnTypeFromIndex(string type, AuthenticationResult authResult)
         {
             var claim = authResult.ClaimsPrincipal.Claims.FirstOrDefault(c => c.Type.Equals(type));
-            return claim.Value;
+            return claim?.Value;
         }
 
     }
