775237500f947ddff12f4721361372b68b72bd71
Nicolas Perruisset <perruisset@insa-toulouse.fr>
2023-01-20 17:18:42 +0100

    add BookedTricycle property in useridentity model

--- INSAT.4I4U.TryShare.MobileApp/Model/UserIdentity.cs
+++ INSAT.4I4U.TryShare.MobileApp/Model/UserIdentity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace INSAT._4I4U.TryShare.MobileApp.Model
+﻿namespace INSAT._4I4U.TryShare.MobileApp.Model
 {
     public class UserIdentity
     {
