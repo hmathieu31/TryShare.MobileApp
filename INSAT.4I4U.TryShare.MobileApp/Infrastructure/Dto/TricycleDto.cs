a6c86ffa99c1738098a360d551801d118e21d060
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2023-01-23 17:29:15 +0100

    ✨ Introduce viewing of ratings

--- INSAT.4I4U.TryShare.MobileApp/Infrastructure/Dto/TricycleDto.cs
+++ INSAT.4I4U.TryShare.MobileApp/Infrastructure/Dto/TricycleDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,10 @@ namespace INSAT._4I4U.TryShare.MobileApp.Infrastructure.Dto
         public int Id { get; set; }
         public double LastKnownLatitude { get; set; }
         public double LastKnownLongitude { get; set; }
+        [Range(0, 100, ErrorMessage = "Battery percentage must be between 0 and 100")]
         public int BatteryPercentage { get; set; }
         public bool IsAvailable { get; set; }
+        [Range(0, 5, ErrorMessage = "The rating must be between 0 and 5")]
+        public int Rating { get; set; }
     }
 }
