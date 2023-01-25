eed02f82a839be31da8d0b53533504d267357746
Ger0th <aubutguillaume@gmail.com>
2023-01-25 13:42:49 +0100

    add screen lock and splash screen

--- INSAT.4I4U.TryShare.MobileApp/Platforms/Android/MainActivity.cs
+++ INSAT.4I4U.TryShare.MobileApp/Platforms/Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace INSAT._4I4U.TryShare.MobileApp.Platforms.Android;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, ScreenOrientation = ScreenOrientation.Portrait)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
